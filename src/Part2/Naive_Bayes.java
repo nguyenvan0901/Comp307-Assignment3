426d608a70caf386da3fee361e15859801ecc435
nguyenvan0901 <vanbanguyen0901@gmail.com>
2022-05-09 19:14:56 +1200

    finalised the program

--- src/Part2/Naive_Bayes.java
+++ src/Part2/Naive_Bayes.java
@@ -36,11 +36,10 @@ public class Naive_Bayes {
 
 
     public Naive_Bayes(){
-        System.out.println("--------------------------------------------------------------------------------------------------------");
-        File file = new File("src/Part2/breast-cancer-test.csv");
+        File train_file = new File("src/Part2/breast-cancer-training.csv");
 
         try{
-            Scanner sc = new Scanner(file);
+            Scanner sc = new Scanner(train_file);
 
             String attributes_string = sc.next();
             String[] attributes = attributes_string.split(",");
@@ -89,11 +88,47 @@ public class Naive_Bayes {
             System.out.println(attribute_names.get(i) + ": " + attribute_values.get(i).toString());
 
         }
-
-        System.out.println("--------------------------------------------------------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------------------------");
         // -------------------------------Checking----------------------------------------------------------------------
 
+
+
+        // -------------------------------Training the Naive Bayes model------------------------------------------------
         this.trainNB();
+        // -------------------------------Training the Naive Bayes model------------------------------------------------
+
+
+
+        // -------------------------------Constructing test instances---------------------------------------------------
+        File test_file  = new File("src/Part2/breast-cancer-test.csv");
+
+        List<Instance> test_instances = new ArrayList<>();
+        try{
+
+            Scanner sc = new Scanner(test_file);
+
+            // skipping the first line of the file (contains attribute names).
+            sc.next();
+
+            while(sc.hasNext()){
+
+                String line = sc.next();
+                String[] parts = line.split(",");
+
+                test_instances.add(new Instance(parts));
+
+            }
+
+        }catch(Exception e){
+            System.out.println("Something is wrong with test file");
+        }
+        // -------------------------------Constructing test instances---------------------------------------------------
+
+
+        // -------------------------------Perform predictions-----------------------------------------------------------
+        System.out.println("Perform predictions: \n");
+        this.predict(test_instances);
+        // -------------------------------Perform predictions-----------------------------------------------------------
 
     }
 
@@ -102,7 +137,7 @@ public class Naive_Bayes {
         this.initialiseCount();
         this.updateCount();
         this.updateTotal();
-        //this.calculateProbability();
+        this.calculateProbability();
 
     }
 
@@ -142,7 +177,6 @@ public class Naive_Bayes {
     }
 
     public void updateCount(){
-        HashMap<String, Integer> count = counts.get(0);
 
         for(Instance instance: train_instances){
 
@@ -164,14 +198,16 @@ public class Naive_Bayes {
             }
 
         }
+//        System.out.println("Count of (Xi=x, Y=y) \n");
+//        for(HashMap<String, Integer> hm: counts){
+//            for(Map.Entry<String, Integer> entry: hm.entrySet()){
+//                System.out.println(entry.toString());
+//            }
+//
+//            System.out.println("\n");
+//        }
 
-        for(HashMap<String, Integer> hm: counts){
-            for(Map.Entry<String, Integer> entry: hm.entrySet()){
-                System.out.println(entry.toString());
-            }
-
-            System.out.println("\n");
-        }
+        System.out.println("--------------------------------------------------------------------------------------------");
 
     }
 
@@ -202,7 +238,7 @@ public class Naive_Bayes {
                 }
 
                 // key is of form (Xi,y)
-                String key = attribute + "," + label;
+                String key = attribute + ",class=" + label;
                 total.put(key, count);
 
             }
@@ -210,9 +246,9 @@ public class Naive_Bayes {
         }
 
         //------------------This code is only for checking----------------------
-        for(Map.Entry<String, Integer> entry: total.entrySet()){
-            System.out.println(entry.toString());
-        }
+//        for(Map.Entry<String, Integer> entry: total.entrySet()){
+//            System.out.println(entry.toString());
+//        }
 //
 //        System.out.println("\n");
 //
@@ -221,15 +257,15 @@ public class Naive_Bayes {
 //        }
         //------------------This code is only for checking----------------------
 
-        System.out.println("class total: " + class_total);
+//        System.out.println("class total: " + class_total);
     }
 
     public void calculateProbability(){
 
         for(String label: distinct_labels){
 
-            double probability = (double) y_count.get(label) / (double) class_total;
-            probability_y.put(label, probability);
+            double y_probability = (double) y_count.get(label) / (double) class_total;
+            probability_y.put(label, y_probability);
 
             for(int i=0; i<attribute_names.size(); i++){
 
@@ -237,16 +273,109 @@ public class Naive_Bayes {
 
                 for(String xi: attribute_values.get(i)){
 
+                    String count_key = attribute_name + "=" + xi + ",class=" + label;
+                    String total_key = attribute_name + ",class=" + label;
+                    String prob_key  = attribute_name + "=" + xi + "|class=" + label;
 
+
+                    int count = counts.get(i).get(count_key);
+
+                    int total2 = total.get(total_key);
+
+                    double probability = (double) count / (double) total2;
+
+                    probability_table.put(prob_key, probability);
 
                 }
 
             }
 
         }
+//        System.out.println("-----------------------------------------------------------------------------------------");
+//        System.out.println("Probability of y");
+//        for(Map.Entry<String, Double> entry: probability_y.entrySet()){
+//            System.out.println(entry.toString());
+//        }
+//
+//        System.out.println("\n");
+//
+//        System.out.println("Conditional probability");
+//        for(Map.Entry<String, Double> entry: probability_table.entrySet()){
+//            System.out.println(entry.toString());
+//        }
+//
+//        System.out.println("-----------------------------------------------------------------------------------------");
 
     }
 
+
+    public double calculateClassScore(Instance instance, String label){
+        double score = probability_y.get(label);
+
+        List<String> values = instance.getValues();
+
+        for(int i=0; i<values.size(); i++){
+
+            String attribute_name  = attribute_names.get(i);
+            String attribute_value = values.get(i);
+
+            String probability_key = attribute_name + "=" + attribute_value + "|class=" + label;
+
+            double probability = probability_table.get(probability_key);
+
+            score = score * probability;
+
+        }
+        return score;
+    }
+
+    public void predict(List<Instance> test_instances){
+        List<String> test_label = new ArrayList<>();
+        List<String> predicted_label = new ArrayList<>();
+
+        for(Instance instance: test_instances){
+            test_label.add(instance.getLabel());
+        }
+
+        for(Instance instance: test_instances){
+
+            double final_score = 0.0;
+            String final_label = "";
+
+            for(String label: distinct_labels){
+
+                double score = calculateClassScore(instance, label);
+
+                if(score > final_score){
+
+                    final_score = score;
+                    final_label = label;
+
+                }
+
+            }
+
+            predicted_label.add(final_label);
+        }
+
+        System.out.println(test_label.toString());
+        System.out.println(predicted_label.toString());
+
+        int correct_prediction = 0;
+        for(int i=0; i<test_label.size(); i++){
+
+            if(test_label.get(i).equals(predicted_label.get(i))){
+                correct_prediction++;
+            }
+
+        }
+
+        double accuracy = (double)correct_prediction / (double) test_label.size();
+        System.out.println("Accuracy: " + accuracy + " with " + correct_prediction + "/" + test_instances.size()
+                            + " correct predictions.");
+
+
+    }
 
 
     public static void main(String[] args){
